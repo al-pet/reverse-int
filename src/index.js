c3fe7830ef42d8833a6196c9490c4d6b25465fc9
al-pet <apetrov1024@gmail.com>
2023-01-27 23:35:57 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let nToString = n.toString();
+  let result = "";
+  for (let i = 0; i < nToString.length; i++) {
+    result = `${nToString[i]}${result}`;
+  }
+  const resultInt = parseInt(result,10)
+
+  return resultInt;
+
 }
